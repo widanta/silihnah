c515b8695fb686b74e1a8bd7da6594227d4f973f
widanta <widanta66@gmail.com>
2024-12-12 22:08:59 +0800

    change new format sql

--- silihnah.sql
+++ silihnah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 11 Des 2024 pada 08.44
+-- Waktu pembuatan: 12 Des 2024 pada 14.07
 -- Versi server: 8.0.30
 -- Versi PHP: 8.1.10
 
@@ -36,6 +36,27 @@ CREATE TABLE `barang` (
   `id_petugas` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama`, `stok`, `waktu`, `id_kategori`, `id_petugas`) VALUES
+(2, 'Cuk', 30, '2024-12-11 16:00:00', 3, 1),
+(3, 'Mouse', 3333, '2024-12-11 16:00:00', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_peminjaman`
+--
+
+CREATE TABLE `detail_peminjaman` (
+  `id_detail_peminjaman` int NOT NULL,
+  `id_peminjaman` int NOT NULL,
+  `id_barang` int NOT NULL,
+  `jumlah` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +69,15 @@ CREATE TABLE `kategori` (
   `deskripsi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama`, `deskripsi`) VALUES
+(3, 'Elektronik', 'kategori elektronik'),
+(5, 'scsc', 'dsav'),
+(6, 'sdvdsv', 'dsvdsvsdvdsvsd                  5     ');
+
 -- --------------------------------------------------------
 
 --
@@ -55,15 +85,23 @@ CREATE TABLE `kategori` (
 --
 
 CREATE TABLE `mahasiswa` (
-  `nim` varchar(10) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `telpon` int NOT NULL,
-  `jenis_kelamin` enum('Laki-Laki','Perempuan') NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `alamat` text NOT NULL,
-  `agama` enum('Hindu','Islam','Budha','Kristen','Katolik','Konghucu') NOT NULL,
-  `id_user` int NOT NULL
+  `id_mahasiswa` int NOT NULL,
+  `id_user` int NOT NULL,
+  `nim` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `telpon` int DEFAULT NULL,
+  `jenis_kelamin` enum('Laki-Laki','Perempuan') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `agama` enum('Hindu','Islam','Budha','Kristen','Katolik','Konghucu') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `id_user`, `nim`, `nama`, `telpon`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `agama`) VALUES
+(1, 1, '332', 'fdsvds', 222, 'Laki-Laki', '2024-12-12', 'dsvujsabcjsacbajcbasjkcbsajkcbajscbajskcbsajkbcsajkbcsajkcbsa', 'Hindu');
 
 -- --------------------------------------------------------
 
@@ -73,12 +111,13 @@ CREATE TABLE `mahasiswa` (
 
 CREATE TABLE `peminjaman` (
   `id_peminjaman` int NOT NULL,
-  `nim` varchar(10) NOT NULL,
+  `id_mahasiswa` int NOT NULL,
   `id_barang` int NOT NULL,
   `jumlah` int NOT NULL,
   `tanggal_pinjam` date NOT NULL,
   `tanggal_kembali` date NOT NULL,
-  `id_petugas` int NOT NULL
+  `id_petugas` int NOT NULL,
+  `id_status` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -90,7 +129,8 @@ CREATE TABLE `peminjaman` (
 CREATE TABLE `pengembalian` (
   `id_pengembalian` int NOT NULL,
   `id_peminjaman` int NOT NULL,
-  `status` enum('Belum Kembali','Sudah Kembali') NOT NULL
+  `id_status` int NOT NULL,
+  `id_petugas` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -101,12 +141,21 @@ CREATE TABLE `pengembalian` (
 
 CREATE TABLE `petugas` (
   `id_petugas` int NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `telpon` int NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `alamat` text NOT NULL,
-  `id_user` int NOT NULL
+  `id_user` int NOT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `telpon` varchar(13) DEFAULT NULL,
+  `jenis_kelamin` enum('Laki-Laki','Perempuan') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `agama` enum('Hindu','Islam','Budha','Kristen','Katolik','Konghucu') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `id_user`, `nama`, `telpon`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `agama`) VALUES
+(1, 4, 'I Made Widanta Abdi Nugraha', '087784793721', 'Laki-Laki', '2005-04-28', 'Jalan Sulatri No 14', 'Hindu');
 
 -- --------------------------------------------------------
 
@@ -127,7 +176,29 @@ CREATE TABLE `role` (
 INSERT INTO `role` (`id_role`, `nama`, `deskripsi`) VALUES
 (1, 'Administrator', 'role admin       dsvds                         '),
 (2, 'Petugas', 'role pertugas'),
-(3, 'Mahasiswa', 'role mahasiswa');
+(3, 'Mahasiswa', 'role mahasiswa'),
+(7, 'dsvdsdsvdsav', '3                      ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `status`
+--
+
+CREATE TABLE `status` (
+  `id_status` int NOT NULL,
+  `nama_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `status`
+--
+
+INSERT INTO `status` (`id_status`, `nama_status`) VALUES
+(1, 'Batal'),
+(2, 'Menunggu Konfirmasi'),
+(3, 'Sedang Dipinjam'),
+(4, 'Telah Dikembalikan');
 
 -- --------------------------------------------------------
 
@@ -148,8 +219,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `id_role`) VALUES
-(1, 'administrator', 'admin@silihnah.com', '$2y$10$2XaWtWMqZ0H1TFyHmwLXIukMGA1JUIj4RtjT62lfNyFia6wv7T8aW', 1),
-(2, 'danta', 'danta@gmail.com', '$2y$10$Fz/22YpuDLvTquXgOb.Awu.obUrG1dAh/txojZaFx4UrqGooZsQ5q', 3);
+(1, 'danta', 'danta@gmail.com', '$2y$10$w7Yokt4UmTZgRAoiYqTa9OqDO3br7Gg.1o/5DzK9NzJpGmTI0rwjO', 3),
+(2, 'admin', 'admin@silihnah.com', '$2y$10$WcTWpJ4e0lBDbhNA7LW4QugFA2siPSQkTEKxKUpiJ/memeQJYEPPG', 3),
+(3, 'sdvsad', 'superadmin@gmail.com', '$2y$10$wQbTewfpGSHxX1/GFMLRk.0hfxsgB.pkDP8Dr1erJX/wvTDFZ7lq2', 1),
+(4, 'tata', 'tata@gmail.com', '$2y$10$rMwYAbimZ.g6pmFxBMwCeembjY53XAgXGP042QDwwd/fMnM3BJt.q', 1);
 
 --
 -- Indexes for dumped tables
@@ -164,6 +237,14 @@ ALTER TABLE `barang`
   ADD KEY `id_kategori` (`id_kategori`);
 
 --
+-- Indeks untuk tabel `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  ADD PRIMARY KEY (`id_detail_peminjaman`),
+  ADD KEY `id_peminjaman` (`id_peminjaman`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
 -- Indeks untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
@@ -173,7 +254,7 @@ ALTER TABLE `kategori`
 -- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`nim`),
+  ADD PRIMARY KEY (`id_mahasiswa`),
   ADD KEY `id_user` (`id_user`);
 
 --
@@ -181,16 +262,20 @@ ALTER TABLE `mahasiswa`
 --
 ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`id_peminjaman`),
-  ADD KEY `nim` (`nim`),
+  ADD KEY `nim` (`id_mahasiswa`),
   ADD KEY `id_barang` (`id_barang`),
-  ADD KEY `id_petugas` (`id_petugas`);
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `id_mahasiswa` (`id_mahasiswa`),
+  ADD KEY `id_status` (`id_status`);
 
 --
 -- Indeks untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD PRIMARY KEY (`id_pengembalian`),
-  ADD KEY `id_peminjaman` (`id_peminjaman`);
+  ADD KEY `id_peminjaman` (`id_peminjaman`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `id_status` (`id_status`);
 
 --
 -- Indeks untuk tabel `petugas`
@@ -204,6 +289,12 @@ ALTER TABLE `petugas`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indeks untuk tabel `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id_status`);
 
 --
 -- Indeks untuk tabel `user`
@@ -220,13 +311,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  MODIFY `id_detail_peminjaman` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `peminjaman`
@@ -244,19 +347,25 @@ ALTER TABLE `pengembalian`
 -- AUTO_INCREMENT untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id_petugas` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_petugas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `role`
 --
 ALTER TABLE `role`
-  MODIFY `id_role` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_role` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `status`
+--
+ALTER TABLE `status`
+  MODIFY `id_status` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -266,13 +375,15 @@ ALTER TABLE `user`
 -- Ketidakleluasaan untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `kategori`
+-- Ketidakleluasaan untuk tabel `detail_peminjaman`
 --
-ALTER TABLE `kategori`
-  ADD CONSTRAINT `kategori_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `barang` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `detail_peminjaman`
+  ADD CONSTRAINT `detail_peminjaman_ibfk_1` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_peminjaman_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `mahasiswa`
@@ -285,13 +396,17 @@ ALTER TABLE `mahasiswa`
 --
 ALTER TABLE `peminjaman`
   ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peminjaman_ibfk_3` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id_mahasiswa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peminjaman_ibfk_4` FOREIGN KEY (`id_status`) REFERENCES `status` (`id_status`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
-  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pengembalian_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pengembalian_ibfk_3` FOREIGN KEY (`id_status`) REFERENCES `status` (`id_status`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `petugas`
